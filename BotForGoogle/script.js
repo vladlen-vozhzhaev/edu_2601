ef2588e9d8e34d5ab7c1f229b8d3c7d451554a86
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2021-04-21 21:20:05 +0300

    Update script.js
    Добавлена работа с несколькими сайтами

--- BotForGoogle/script.js
+++ BotForGoogle/script.js
@@ -5,22 +5,45 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.google.com/*
+// @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
+// @match        https://crushdrummers.ru/*
 // @icon         https://www.google.com/s2/favicons?domain=google.com
 // @grant        none
 // ==/UserScript==
-let keywords = ["Гобой","Флейта","Как звучить флейта","Фагот","Кларнет"];
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+let sites = {
+    "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai":["Гобой","Флейта","Как звучить флейта","Фагот","Кларнет"],
+    "crushdrummers.ru":["барабанное шоу crush","crushDrummers","шоу барабанщиков crush"]
+}
+let site = Object.keys(sites)[Math.floor(Math.random()*Object.keys(sites).length)];
+let keywords = sites[site];
 let keyword = keywords[Math.floor(Math.random()*keywords.length)];
 let btnK = document.getElementsByName('btnK')[1];
+let googleInput = document.getElementsByName('q')[0];
 if(btnK!=undefined){ // Главная страница google или нет
-    document.getElementsByName('q')[0].value = keyword;
-    btnK.click();
-}else{ // Если не на гланвой
+    document.cookie = "site="+site;
+    let i =0;
+    let timerId = setInterval(function(){
+        googleInput.value += keyword[i++]
+        if(keyword.length == i){
+            clearInterval(timerId);
+            btnK.click();
+        }
+    },500);
+}else if(location.hostname === "www.google.com"){ // Если не на гланвой, но всё ещё на сайте google
     let links = document.links;
     let goToTheNextPage = true;
     let currentPage = document.getElementsByClassName("YyVfkd")[0].innerText;
+    site = getCookie("site");
     for(let i=0; i<links.length; i++){
         let link = links[i];
-        if(link.href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai') != -1){
+        if(link.href.indexOf(site) != -1){
             link.click();
             goToTheNextPage = false;
             break;
@@ -28,4 +51,12 @@ if(btnK!=undefined){ // Главная страница google или нет
     }
     if(currentPage>10) location.href = "https://www.google.com/";
     else if (goToTheNextPage) pnnext.click();
+}else{
+    if(Math.random() > 0.8) location.href = "https://www.google.com/";
+    let links = document.links;
+    setInterval(function(){
+        let index = Math.floor(Math.random()*links.length)
+        let link = links[index];
+        if(link.href.includes(location.hostname)) link.click();
+    },3000);
 }
